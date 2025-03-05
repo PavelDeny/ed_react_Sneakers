aa71f11a41f668a76c6bc966f4279b31644e9274
Pavel D. <paveldenysenko91@gmail.com>
2025-03-05 23:19:10 +0200

    scss module

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,18 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/hurt_nofill.svg" alt="hurt_nofill" />
       </div>
 
-      <img
-        width={133}
-        height={112}
-        src="/img/sneakers/image1.jpg"
-        //  src=""
-        alt="sneakers"
-      />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="plus" />
